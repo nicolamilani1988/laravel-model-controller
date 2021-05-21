3e11160902e0fc685162bad509a5d595ec80378e
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-05-21 16:28:38 +0200

    modal creato e db collegato correttamente

--- Movie_db_project/app/Http/Controllers/TestController.php
+++ Movie_db_project/app/Http/Controllers/TestController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movies;
 
 class TestController extends Controller
 {
     public function home(){
+        $movies = Movies::all();
+        dd($movies);
         return view('pages.homepage');
     }
 }
